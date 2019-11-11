95575709da84eaf0f17d18c77870d21ddfc65f39
Przemyslaw Kaleta <przemek2889@gmail.com>
2019-11-11 21:31:32 +0100

    Implemented sequential BFS.

---
+++ src/bfs_cpu.cpp
@@ -0,0 +1,24 @@
+#include "bfs_cpu.h"
+
+using namespace std;
+
+
+void bfsCPU(int start, Graph &G, std::vector<int> &distance, std::vector<bool> &visited) {
+	distance[start] = 0;
+	visited[start] = true;
+	queue<int> to_visit;
+	to_visit.push(start);
+
+	while (!to_visit.empty()) {
+		int current = to_visit.front();
+		to_visit.pop();
+		for (int i = G.edgesOffset[current]; i < G.edgesOffset[current] + G.edgesSize[current]; ++i) {
+			int v = G.adjacencyList[i];
+			if (!visited[v]) {
+				distance[v] = distance[current] + 1;
+				visited[v] = true;
+				to_visit.push(v);
+			}
+		}
+	}
+}
---
+++ src/bfs_cpu.h
@@ -0,0 +1,15 @@
+#ifndef BFS_CPU_H
+#define BFS_CPU_H
+
+#include "graph.h"
+#include <queue>
+
+/*
+ * start - vertex number from which traversing a graph starts
+ * G - graph to traverse
+ * distance - placeholder for vector of distances (filled after invoking a function)
+ * visited - placeholder for vector indicating that vertex was visited (filled after invoking a function)
+ */
+void bfsCPU(int start, Graph &G, std::vector<int> &distance, std::vector<bool> &visited);
+
+#endif // BFS_CPU_H
