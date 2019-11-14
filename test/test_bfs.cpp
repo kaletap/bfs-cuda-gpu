6fd3987db36bae3ad0b052fa43c8329681ec0859
Przemyslaw Kaleta <przemek2889@gmail.com>
2019-11-14 16:43:32 +0100

    Added more gpu bfs tests.

--- test/test_bfs.cpp
+++ test/test_bfs.cpp
@@ -16,8 +16,6 @@ protected:
 		graph = Graph(5, {{0,1}, {0, 2}, {1, 2}, {2, 3}, {3, 4}});
 		distance = vector<int>(graph.numVertices);
 		visited = vector<bool>(graph.numVertices);
-		start = 0;
-		expectedDistance = vector<int>( {0, 1, 1, 2, 3} );
 		expectedVisited = vector<bool>(graph.numVertices, true);
 	}
 	Graph graph;
@@ -29,17 +27,48 @@ protected:
 };
 
 
-TEST_F(BFSTest, BFS_CPU_Test) {
+TEST_F(BFSTest, BFS_CPU_Test_start0) {
+	start = 0;
+	expectedDistance = vector<int>( {0, 1, 1, 2, 3} );
 	bfsCPU(start, graph, distance, visited);
-	DEBUG(print(distance));
 	EXPECT_EQ(expectedDistance, distance);
 	EXPECT_EQ(expectedVisited, visited);
 }
 
 
-TEST_F(BFSTest, BFS_Simple_GPU_Test) {
+TEST_F(BFSTest, BFS_CPU_Test_start1) {
+	start = 1;
+	expectedDistance = vector<int>( {1, 0, 1, 2, 3} );
+	bfsCPU(start, graph, distance, visited);
+	EXPECT_EQ(expectedDistance, distance);
+	EXPECT_EQ(expectedVisited, visited);
+}
+
+
+TEST_F(BFSTest, BFS_CPU_Test_start2) {
+	start = 2;
+	expectedDistance = vector<int>( {1, 1, 0, 1, 2} );
+	bfsCPU(start, graph, distance, visited);
+	EXPECT_EQ(expectedDistance, distance);
+	EXPECT_EQ(expectedVisited, visited);
+}
+
+
+TEST_F(BFSTest, BFS_Simple_GPU_Test_start0) {
+	start = 0;
+	expectedDistance = vector<int>( {0, 1, 1, 2, 3} );
 	bfsGPU(start, graph, distance, visited);
 	DEBUG(print(distance));
 	EXPECT_EQ(expectedDistance, distance);
-	EXPECT_EQ(expectedVisited, visited);
+//	EXPECT_EQ(expectedVisited, visited);
+}
+
+
+TEST_F(BFSTest, BFS_Simple_GPU_Test_start1) {
+	start = 1;
+	expectedDistance = vector<int>( {1, 0, 1, 2, 3} );
+	bfsGPU(start, graph, distance, visited);
+	DEBUG(print(distance));
+	EXPECT_EQ(expectedDistance, distance);
+//	EXPECT_EQ(expectedVisited, visited);
 }
