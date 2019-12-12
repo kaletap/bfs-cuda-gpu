9fe45cce06496abfba8a169dfae414cf8e8faffd
Przemyslaw Kaleta <przemek2889@gmail.com>
2019-12-12 16:21:50 +0100

    Added c++11 to compile arguments.

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 OUT_FILE=bfs_main.out
-nvcc src/main.cpp src/gpu/quadratic/bfs_quadratic.cu src/gpu/simple/bfs_simple.cu src/graph/graph.cpp src/cpu/bfs.cpp -o $OUT_FILE \
+nvcc -std=c++11 src/main.cpp src/gpu/quadratic/bfs_quadratic.cu src/gpu/simple/bfs_simple.cu src/graph/graph.cpp src/cpu/bfs.cpp -o $OUT_FILE \
 && echo "Compiled the program and created executable named $OUT_FILE."
 
